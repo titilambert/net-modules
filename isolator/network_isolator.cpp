1b7a3020b55c49fc118a0b8354521e6bbf78357f
Spike Curtis <spike.curtis@metaswitch.com>
2015-05-01 15:58:43 -0700

    Isolator uses ip address from env

--- isolator/network_isolator.cpp
+++ isolator/network_isolator.cpp
@@ -117,7 +117,6 @@ public:
       const ContainerID& containerId,
       pid_t pid)
   {
-
     const Info* info = infos[containerId];
     foreach (const Parameter& parameter, parameters.parameter()) {
       if (parameter.key() == isolateKey) {
@@ -160,6 +159,11 @@ public:
   virtual process::Future<Nothing> cleanup(
       const ContainerID& containerId)
   {
+    if (!infos.contains(containerId)) {
+      LOG(WARNING) << "Ignoring cleanup for unknown container " << containerId;
+      return Nothing();
+    }
+    infos.erase(containerId);
     foreach (const Parameter& parameter, parameters.parameter()) {
       if (parameter.key() == cleanupKey) {
         std::vector<std::string> argv(4);
